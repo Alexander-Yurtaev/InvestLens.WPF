814a280c64947f219bfe29c91a3b154dfea74595
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-04-07 17:34:07 +0300

    Refactoring of DashboardViewModel.

---
+++ src/InvestLens.ViewModel/Pages/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace InvestLens.ViewModel.Pages;
+
+public abstract class BaseViewModel : BindableBase
+{
+    protected BaseViewModel(string title, string description)
+    {
+        ContentHeaderVm = new ContentHeaderViewModel(title, description);
+    }
+
+    public IContentHeaderViewModel ContentHeaderVm { get; private set; }
+}
--- src/InvestLens.ViewModel/Pages/DashboardViewModel.cs
+++ src/InvestLens.ViewModel/Pages/DashboardViewModel.cs
@@ -3,32 +3,21 @@ using InvestLens.ViewModel.Services;
 
 namespace InvestLens.ViewModel.Pages;
 
-public class DashboardViewModel : BindableBase, IDashboardViewModel
+public class DashboardViewModel : BaseViewModel, IDashboardViewModel
 {
-    private readonly IUserManager _userManager;
-    private readonly IMetricsManager _metricsManager;
-    private readonly IActivityManager _activityManager;
-
-    public DashboardViewModel(IUserManager userManager, 
-        IMetricsManager metricsManager, 
+    public DashboardViewModel(IUserManager userManager,
+        IMetricsManager metricsManager,
         IActivityManager activityManager,
-        IPortfolioDynamicsService portfolioDynamicsService)
+        IPortfolioDynamicsService portfolioDynamicsService) : base($"Добро пожаловать, {userManager.UserName}",
+        "Вот что происходит с вашими инвестициями сегодня")
     {
         PortfolioDynamicsService = portfolioDynamicsService;
-        _userManager = userManager;
-        _metricsManager = metricsManager;
-        _activityManager = activityManager;
-        MetricCards = _metricsManager.GetMetricCards();
-        ActivityItems = _activityManager.GetActivityItems();
-
-        var welcomeTitle = $"Добро пожаловать, {_userManager.UserName}";
-        var welcomeDescription = "Вот что происходит с вашими инвестициями сегодня";
-        ContentHeaderVm = new ContentHeaderViewModel(welcomeTitle, welcomeDescription);
+        MetricCards = metricsManager.GetMetricCards();
+        ActivityItems = activityManager.GetActivityItems();
     }
 
     public IPortfolioDynamicsService PortfolioDynamicsService { get; }
 
     public List<MetricCard> MetricCards { get; }
     public List<ActivityItem> ActivityItems { get; }
-    public IContentHeaderViewModel ContentHeaderVm { get; }
 }
